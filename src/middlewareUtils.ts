2b4392f4049d3a7a0901fb1b763c92f200cb60d8
Kait Kasak <kait@snowhound.eu>
2023-08-22 13:53:42 +0300

    0.0.4-beta.0

--- src/middlewareUtils.ts
+++ src/middlewareUtils.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const SECURE_TOKEN_PREFIX = '__Secure';
+export const SECURE_TOKEN_PREFIX = '__Secure-';
 
 export const AUTH_TOKEN_SUFFIX = 'next-auth.session-token';
 
@@ -14,10 +14,19 @@ const getAuthCookie = (request: NextRequest, tokenSuffix: string) => {
 };
 export const setAuthCookieExpiryToSession = (request: NextRequest, options?: TokenExpiryOptions): NextResponse => {
   const { securePrefix = SECURE_TOKEN_PREFIX, tokenSuffix = AUTH_TOKEN_SUFFIX } = options || {};
-  const authCookie = getAuthCookie(request, tokenSuffix);
-  const response = NextResponse.next();
 
+  const path = request.nextUrl.pathname;
+
+  if (path.startsWith('/_next') // exclude Next.js internals
+      || path.startsWith('/api') //  exclude all API routes
+      || path.startsWith('/static')// exclude static files
+  ) {
+    return NextResponse.next();
+  }
+
+  const authCookie = getAuthCookie(request, tokenSuffix);
   if (authCookie?.value) {
+    const response = NextResponse.next();
     response.cookies.set({
       name: authCookie.name,
       value: authCookie.value,
@@ -28,6 +37,8 @@ export const setAuthCookieExpiryToSession = (request: NextRequest, options?: Tok
       httpOnly: true,
       secure: authCookie.name.startsWith(securePrefix),
     });
+    return response;
   }
-  return response;
+
+  return NextResponse.next();
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,18 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const _dirname = typeof __dirname !== 'undefined'
-    ? __dirname
-    : dirname(fileURLToPath(import.meta.url))
-
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(_dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'next-auth-cookie-fix',
       fileName: 'next-auth-cookie-fix',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['next'],
+      external: ['next/server'],
       output: {},
     },
   },
